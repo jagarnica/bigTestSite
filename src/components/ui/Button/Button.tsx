bf275df1869799843d564213aa1606102e278a3f
Jesus Garnica <jgarnica@launchdarkly.com>
2021-09-23 10:00:10 -0700

    Updaing button styles

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -3,7 +3,7 @@ import { Button as CButton, ButtonProps } from "@chakra-ui/react";
 
 export function Button({ children, ...props }: ButtonProps): JSX.Element {
   return (
-    <CButton fontSize="sm" colorScheme="blue" {...props}>
+    <CButton fontSize="xl" colorScheme="teal" {...props}>
       {children}
     </CButton>
   );
